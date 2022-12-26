bbabda09cfe783d05427fe5cbcc0ef43ef3ce56e
Tu Nguyen <nguy1581@gmail.com>
2022-12-26 00:23:48 -0600

    Initial commit Changed database name (rpg_app) Created a World and Inventory component

---
+++ src/components/Inventory/Inventory.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Inventory = () => {
+    return <span> Inventory View</span>;
+};
+
+export default Inventory;
---
+++ src/components/World/World.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const World = () => {
+    return <span> World View</span>;
+};
+
+export default World;
