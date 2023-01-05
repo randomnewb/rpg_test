4e87ffff863a87ac3505bcb84676e36f36223212
randomnewb <nguy1581@gmail.com>
2023-01-04 18:14:44 -0600

    Updated pool.js to work with bit.io

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,18 +1,23 @@
 const pg = require("pg");
-let pool;
 
-// When our app is deployed to the internet
-// we'll use the DATABASE_URL environment variable
-// to set the connection info: web address, username/password, db name
-// eg:
-//  DATABASE_URL=postgresql://jDoe354:secretPw123@some.db.com/prime_app
+let config = {};
+
 if (process.env.DATABASE_URL) {
-  pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
+  // Heroku gives a url, not a connection object
+  // https://github.com/brianc/node-pg-pool
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(":");
+
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: process.env.DATABASE_NAME || params.pathname.split("/")[1],
+    ssl: { rejectUnauthorized: false },
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
 }
 // When we're running this app on our own computer
 // we'll connect to the postgres database that is
@@ -33,5 +38,15 @@ else if (process.env.DATABASE_PASSWORD) {
     database: "rpg_app",
   });
 }
+
+// this creates the pool that will be shared by all other modules
+const pool = new pg.Pool(config);
+
+// the pool with emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on("error", (err) => {
+  console.log("Unexpected error on idle client", err);
+  process.exit(-1);
+});
 
 module.exports = pool;
