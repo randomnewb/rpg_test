0443e780f01683518f78656ef4218132b99df58b
randomnewb <nguy1581@gmail.com>
2023-01-04 20:47:37 -0600

    Pool test

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,12 +1,8 @@
 const pg = require("pg");
-
-if (process.env.DATABASE_URL) {
-  const url = require("url");
-}
-
 let config = {};
 
 if (process.env.DATABASE_URL) {
+  const url = require("url");
   // Heroku gives a url, not a connection object
   // https://github.com/brianc/node-pg-pool
   const params = url.parse(process.env.DATABASE_URL);
@@ -22,6 +18,16 @@ if (process.env.DATABASE_URL) {
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
+
+  // this creates the pool that will be shared by all other modules
+  const pool = new pg.Pool(config);
+
+  // the pool with emit an error on behalf of any idle clients
+  // it contains if a backend error or network partition happens
+  pool.on("error", (err) => {
+    console.log("Unexpected error on idle client", err);
+    process.exit(-1);
+  });
 } else if (
   // When we're running this app on our own computer
   // we'll connect to the postgres database that is
@@ -41,18 +47,6 @@ if (process.env.DATABASE_URL) {
     port: 5432,
     user: "postgres",
     database: "rpg_app",
-  });
-}
-
-if (process.env.DATABASE_URL) {
-  // this creates the pool that will be shared by all other modules
-  const pool = new pg.Pool(config);
-
-  // the pool with emit an error on behalf of any idle clients
-  // it contains if a backend error or network partition happens
-  pool.on("error", (err) => {
-    console.log("Unexpected error on idle client", err);
-    process.exit(-1);
   });
 }
 
