c194ec56c5d31a28d8376c1dcc332ab1e3e418ce
randomnewb <nguy1581@gmail.com>
2023-01-07 22:04:23 -0600

    Disable console.log

--- server/routes/entity.router.js
+++ server/routes/entity.router.js
@@ -2,19 +2,19 @@ const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
 const {
-    rejectUnauthenticated,
+  rejectUnauthenticated,
 } = require("../modules/authentication-middleware");
 
 /**
  * FETCH specific entity by its id
  */
 router.get("/:id", rejectUnauthenticated, async (req, res) => {
-    const db = await pool.connect();
+  const db = await pool.connect();
 
-    try {
-        await db.query("BEGIN");
+  try {
+    await db.query("BEGIN");
 
-        const sql = `
+    const sql = `
         SELECT spawn.current_health, stat.name, stat.type
         FROM spawn spawn, stat stat
         WHERE 
@@ -22,180 +22,178 @@ router.get("/:id", rejectUnauthenticated, async (req, res) => {
         AND spawn.id = $1;
         `;
 
-        result = await db.query(sql, [req.params.id]);
+    result = await db.query(sql, [req.params.id]);
 
-        res.status(200).send(result.rows[0]);
-    } catch (err) {
-        console.log(err);
-        sendStatus(500);
-    } finally {
-        db.release();
-    }
-    // }
+    res.status(200).send(result.rows[0]);
+  } catch (err) {
+    console.log(err);
+    sendStatus(500);
+  } finally {
+    db.release();
+  }
+  // }
 });
 
 /**
  * INTERACT with specific entity by its id
  */
 router.put("/:id", async (req, res) => {
-    // Get the entity by its id
-    // Subtract the entity's current_health by 1
-    // Return the entity
+  // Get the entity by its id
+  // Subtract the entity's current_health by 1
+  // Return the entity
 
-    const db = await pool.connect();
+  const db = await pool.connect();
 
-    const entityExists = await checkEntityExists(req.params.id, db);
+  const entityExists = await checkEntityExists(req.params.id, db);
 
-    console.log("what is entityExists", entityExists);
-
-    if (entityExists === undefined) {
-        try {
-            const sql_setUserState = `
+  if (entityExists === undefined) {
+    try {
+      const sql_setUserState = `
       UPDATE "user" 
       SET current_state='observing'
       WHERE id=$1
       RETURNING current_state;
       `;
 
-            const result = await db.query(sql_setUserState, [req.user.id]);
+      const result = await db.query(sql_setUserState, [req.user.id]);
 
-            res.send(result.rows[0]);
-        } catch (err) {
-            console.error(err);
-            res.sendStatus(500);
-        } finally {
-            db.release();
-        }
-    } else if (req.params.id > 0) {
-        try {
-            const healthOfEntity = await checkHealthOfEntity(req.params.id, db);
-            const damageCalculation = await performDamageCalculation(
-                req.params.id,
-                healthOfEntity,
-                db
-            );
+      res.send(result.rows[0]);
+    } catch (err) {
+      console.error(err);
+      res.sendStatus(500);
+    } finally {
+      db.release();
+    }
+  } else if (req.params.id > 0) {
+    try {
+      const healthOfEntity = await checkHealthOfEntity(req.params.id, db);
+      const damageCalculation = await performDamageCalculation(
+        req.params.id,
+        healthOfEntity,
+        db
+      );
 
-            if (damageCalculation <= 0) {
-                const sql_setUserState = `
+      if (damageCalculation <= 0) {
+        const sql_setUserState = `
       UPDATE "user" 
       SET current_state='observing'
       WHERE id=$1;
       `;
 
-                await db.query(sql_setUserState, [req.user.id]);
+        await db.query(sql_setUserState, [req.user.id]);
 
-                let resetState = { current_state: "observing" };
-                res.status(201).send(resetState);
-            }
+        let resetState = { current_state: "observing" };
+        res.status(201).send(resetState);
+      }
 
-            if (damageCalculation.current_health >= 1) {
-                res.status(201).send(damageCalculation);
-            }
-        } catch (err) {
-            await db.query("ROLLBACK");
-            console.error(err);
-            res.sendStatus(500);
-        } finally {
-            db.release();
-        }
+      if (damageCalculation.current_health >= 1) {
+        res.status(201).send(damageCalculation);
+      }
+    } catch (err) {
+      await db.query("ROLLBACK");
+      console.error(err);
+      res.sendStatus(500);
+    } finally {
+      db.release();
     }
+  }
 });
 
 const checkEntityExists = async (entityId, db) => {
-    // Check to see if the spawned entity the user is interacting with still exists
-    try {
-        const sql_checkEntityExists = `
+  // Check to see if the spawned entity the user is interacting with still exists
+  try {
+    const sql_checkEntityExists = `
     SELECT id
     FROM spawn
     WHERE id=$1;
     `;
 
-        const constCheckEntityExists = await db.query(sql_checkEntityExists, [
-            entityId,
-        ]);
+    const constCheckEntityExists = await db.query(sql_checkEntityExists, [
+      entityId,
+    ]);
 
-        return constCheckEntityExists.rows[0];
-    } catch (err) {
-        console.error(err);
-        res.sendStatus(500);
-    }
+    return constCheckEntityExists.rows[0];
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
 };
 
 const checkHealthOfEntity = async (entityId, db) => {
-    // Get health of the entity
-    try {
-        const sql_checkHealthOfEntity = `
+  // Get health of the entity
+  try {
+    const sql_checkHealthOfEntity = `
       SELECT id, current_health
       FROM spawn
       WHERE id=$1;
     `;
 
-        const checkHealthOfEntity = await db.query(sql_checkHealthOfEntity, [
-            entityId,
-        ]);
+    const checkHealthOfEntity = await db.query(sql_checkHealthOfEntity, [
+      entityId,
+    ]);
 
-        return checkHealthOfEntity.rows[0].current_health;
-    } catch (e) {
-        console.log(e);
-        res.sendStatus(500);
-    }
+    return checkHealthOfEntity.rows[0].current_health;
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
 };
 
 const performDamageCalculation = async (entityId, entityHealth, db) => {
-    // For now, just subtract 1 health from entity
-    // If the entity's health is equal to or less than 0
-    // Remove the entity
-    // Or else just send the entity with updated health
-    entityId = parseInt(entityId);
+  // For now, just subtract 1 health from entity
+  // If the entity's health is equal to or less than 0
+  // Remove the entity
+  // Or else just send the entity with updated health
+  entityId = parseInt(entityId);
 
-    entityHealth = entityHealth - 1;
+  entityHealth = entityHealth - 1;
 
-    if (entityHealth <= 0) {
-        try {
-            await db.query("BEGIN");
+  if (entityHealth <= 0) {
+    try {
+      await db.query("BEGIN");
 
-            const sql_performDamageCalculation = `
+      const sql_performDamageCalculation = `
       DELETE FROM spawn
       WHERE id=$1;
       `;
 
-            await db.query(sql_performDamageCalculation, [entityId]);
+      await db.query(sql_performDamageCalculation, [entityId]);
 
-            await db.query("COMMIT");
+      await db.query("COMMIT");
 
-            let result = 0;
+      let result = 0;
 
-            return result;
-        } catch (e) {
-            await db.query("ROLLBACK");
-            console.log(e);
-            res.sendStatus(500);
-        }
-    } else if (entityHealth >= 1) {
-        try {
-            await db.query("BEGIN");
+      return result;
+    } catch (e) {
+      await db.query("ROLLBACK");
+      console.log(e);
+      res.sendStatus(500);
+    }
+  } else if (entityHealth >= 1) {
+    try {
+      await db.query("BEGIN");
 
-            const sql_performDamageCalculation = `
+      const sql_performDamageCalculation = `
         UPDATE spawn
         SET current_health=$2
         WHERE id=$1
         RETURNING *;
         `;
 
-            const damageCalculation = await db.query(
-                sql_performDamageCalculation,
-                [entityId, entityHealth]
-            );
+      const damageCalculation = await db.query(sql_performDamageCalculation, [
+        entityId,
+        entityHealth,
+      ]);
 
-            await db.query("COMMIT");
+      await db.query("COMMIT");
 
-            return damageCalculation.rows[0];
-        } catch (e) {
-            await db.query("ROLLBACK");
-            console.log(e);
-            res.sendStatus(500);
-        }
+      return damageCalculation.rows[0];
+    } catch (e) {
+      await db.query("ROLLBACK");
+      console.log(e);
+      res.sendStatus(500);
     }
+  }
 };
 
 module.exports = router;
--- src/redux/sagas/entity.saga.js
+++ src/redux/sagas/entity.saga.js
@@ -2,59 +2,49 @@ import axios from "axios";
 import { put, takeLatest } from "redux-saga/effects";
 
 function* entitySaga() {
-    yield takeLatest("FETCH_ENTITY_DETAIL", fetchEntityDetail);
-    yield takeLatest("INTERACT_WITH_ENTITY", interactWithEntity);
+  yield takeLatest("FETCH_ENTITY_DETAIL", fetchEntityDetail);
+  yield takeLatest("INTERACT_WITH_ENTITY", interactWithEntity);
 }
 
 function* fetchEntityDetail(action) {
-    if (action.payload === null) {
+  if (action.payload === null) {
+    yield put({
+      type: "UPDATE_USER_STATE",
+      payload: { userState: "observing" },
+    });
+  } else {
+    try {
+      const entity = yield axios.get(`/api/entity/${action.payload}`);
+      if (entity.data === 0) {
         yield put({
-            type: "UPDATE_USER_STATE",
-            payload: { userState: "observing" },
+          type: "UPDATE_USER_STATE",
+          payload: { userState: "observing" },
         });
-    } else {
-        try {
-            const entity = yield axios.get(`/api/entity/${action.payload}`);
-            if (entity.data === 0) {
-                yield put({
-                    type: "UPDATE_USER_STATE",
-                    payload: { userState: "observing" },
-                });
-            } else {
-                yield put({ type: "SET_ENTITY", payload: entity.data });
-            }
-        } catch (e) {
-            console.log("Error fetching entity detail", e);
-            alert("Error fetching specific entity");
-        }
+      } else {
+        yield put({ type: "SET_ENTITY", payload: entity.data });
+      }
+    } catch (e) {
+      console.log("Error fetching entity detail", e);
+      alert("Error fetching specific entity");
     }
+  }
 }
 
 function* interactWithEntity(action) {
-    try {
-        const entity = yield axios.put(`/api/entity/${action.payload}`);
-        console.log("what comes back from entity interact", entity.data);
-        if (entity.data.current_state === "observing" || undefined) {
-            console.log(
-                "entity.data.current_state is",
-                entity.data.current_state
-            );
-            yield put({
-                type: "UPDATE_USER_STATE",
-                payload: { userState: "observing" },
-            });
-        } else {
-            console.log(
-                "current returning state is",
-                entity.data.current_state
-            );
-            console.log("this should not run");
-            yield put({ type: "FETCH_ENTITY_DETAIL", payload: action.payload });
-        }
-    } catch (e) {
-        console.log("Error interacting with entity", e);
-        alert("Error interacting with entity");
+  try {
+    const entity = yield axios.put(`/api/entity/${action.payload}`);
+    if (entity.data.current_state === "observing" || undefined) {
+      yield put({
+        type: "UPDATE_USER_STATE",
+        payload: { userState: "observing" },
+      });
+    } else {
+      yield put({ type: "FETCH_ENTITY_DETAIL", payload: action.payload });
     }
+  } catch (e) {
+    console.log("Error interacting with entity", e);
+    alert("Error interacting with entity");
+  }
 }
 
 export default entitySaga;
