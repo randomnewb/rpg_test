bc005dcafa53e64d001d33a17970f30c5c80d0bf
Tu Nguyen <nguy1581@gmail.com>
2023-01-14 22:20:06 -0600

    GET request for inventory by user

--- server/routes/inventory.router.js
+++ server/routes/inventory.router.js
@@ -2,25 +2,27 @@ const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
 const {
-  rejectUnauthenticated,
+    rejectUnauthenticated,
 } = require("../modules/authentication-middleware");
 
 // GET player's inventory
 
 router.get("/", rejectUnauthenticated, (req, res) => {
-  const sql = `
-        
-        `;
-
-  pool
-    .query(sql, [req.user.id])
-    .then((result) => {
-      res.status(200).send(result.rows[0]);
-    })
-    .catch((e) => {
-      console.log("Error getting player's inventory", e);
-      console.log(500);
-    });
+    const sql = `
+      SELECT item.*, inventory.*
+      FROM item , inventory , "user" 
+      WHERE 
+      "user".id = $1
+      AND inventory.item_id = item.id
+      AND inventory.user_id = "user".id;`;
+    pool.query(sql, [req.user.id])
+        .then((result) => {
+            res.status(200).send(result.rows[0]);
+        })
+        .catch((e) => {
+            console.log("Error getting player's inventory", e);
+            console.log(500);
+        });
 });
 
 module.exports = router;
