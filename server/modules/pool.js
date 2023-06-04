e5851b6ead8544c86e132cff434b8e2c93b71224
randomnewb <nguy1581@gmail.com>
2023-06-03 23:55:13 -0500

    update pool for heroku

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,8 +1,16 @@
+/* the only line you likely need to change is
+
+ database: 'prime_app',
+
+ change `prime_app` to the name of your database, and you should be all set!
+*/
+
 const pg = require("pg");
+const url = require("url");
+
 let config = {};
 
 if (process.env.DATABASE_URL) {
-  const url = require("url");
   // Heroku gives a url, not a connection object
   // https://github.com/brianc/node-pg-pool
   const params = url.parse(process.env.DATABASE_URL);
@@ -18,36 +26,38 @@ if (process.env.DATABASE_URL) {
     max: 10, // max number of clients in the pool
     idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
   };
+} else if (process.env.HEROKU_POSTGRESQL_ORANGE_URL) {
+  const params = url.parse(process.env.HEROKU_POSTGRESQL_ORANGE_URL);
+  const auth = params.auth.split(":");
 
-  // this creates the pool that will be shared by all other modules
-  pool = new pg.Pool(config);
-
-  // the pool with emit an error on behalf of any idle clients
-  // it contains if a backend error or network partition happens
-  pool.on("error", (err) => {
-    console.log("Unexpected error on idle client", err);
-    process.exit(-1);
-  });
-} else if (
-  // When we're running this app on our own computer
-  // we'll connect to the postgres database that is
-  // also running on our computer (localhost)
-  process.env.DATABASE_PASSWORD
-) {
-  pool = new pg.Pool({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    database: "rpg_app",
-    password: process.env.DATABASE_PASSWORD,
-  });
+  config = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: process.env.DATABASE_NAME || params.pathname.split("/")[1],
+    ssl: { rejectUnauthorized: false },
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
 } else {
-  pool = new pg.Pool({
-    host: "localhost",
-    port: 5432,
-    user: "postgres",
-    database: "rpg_app",
-  });
+  config = {
+    host: "localhost", // Server hosting the postgres database
+    port: 5432, // env var: PGPORT
+    database: "prime_app", // CHANGE THIS LINE! env var: PGDATABASE, this is likely the one thing you need to change to get up and running
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+  };
 }
+
+// this creates the pool that will be shared by all other modules
+const pool = new pg.Pool(config);
+
+// the pool with emit an error on behalf of any idle clients
+// it contains if a backend error or network partition happens
+pool.on("error", (err) => {
+  console.log("Unexpected error on idle client", err);
+  process.exit(-1);
+});
 
 module.exports = pool;
